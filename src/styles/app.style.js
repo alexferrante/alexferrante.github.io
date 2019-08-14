336893c634b6c52dedbd54bdea3b9d40a7b28bc2
alexferrante <36389437+alexferrante@users.noreply.github.com>
2019-08-14 15:54:40 -0400

    feat: style

--- src/styles/app.style.js
+++ src/styles/app.style.js
@@ -22,9 +22,12 @@ s.main = {
   fontFamily: 'IBM Plex Sans, sans-serif',
   fontWeight: '300',
   fontSize: '16px',
+  marginLeft: '5%',
   letterSpacing: '0.025em',
-  width: '80%',
-  alignItems: 'flex-end'
+  width: '85%',
+  height: '100%',
+  float: 'right',
+  position: 'relative'
 };
 
 s.title = {
@@ -41,11 +44,25 @@ s.breadcrumbs = {
 s.menu = {
   position: 'absolute',
   right: '400',
-  height: '80vw',
+  height: '60%',
+  maxHeight: '60vw',
   backgroundColor: '#000',
   width: '10%',
-  padding: '10vw 0vw 10vw 0vw',
-  alignSelf: 'text-start',
+  minWidth: '12vw',
+  margin: '18vw 0vw 12vw 0vw',
+  float: 'left'
+}
+
+s.fullMenu = {
+  position: 'absolute',
+  right: '400',
+  height: '60%',
+  maxHeight: '60vw',
+  backgroundColor: '#fff',
+  width: '50%',
+  minWidth: '12vw',
+  margin: '18vw 0vw 12vw 0vw',
+  float: 'left'
 }
 
 export default s;
