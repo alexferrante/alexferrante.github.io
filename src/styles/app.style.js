535f4ce44fb9256c36b232cb9698be8fb2ed7ec1
alexferrante <36389437+alexferrante@users.noreply.github.com>
2019-08-16 11:08:26 -0400

    feat: styling

--- src/styles/app.style.js
+++ src/styles/app.style.js
@@ -41,28 +41,4 @@ s.breadcrumbs = {
   margin: '3vh 0',
 };
 
-s.menu = {
-  position: 'absolute',
-  right: '400',
-  height: '60%',
-  maxHeight: '60vw',
-  backgroundColor: '#000',
-  width: '10%',
-  minWidth: '12vw',
-  margin: '18vw 0vw 12vw 0vw',
-  float: 'left'
-}
-
-s.fullMenu = {
-  position: 'absolute',
-  right: '400',
-  height: '60%',
-  maxHeight: '60vw',
-  backgroundColor: '#fff',
-  width: '50%',
-  minWidth: '12vw',
-  margin: '18vw 0vw 12vw 0vw',
-  float: 'left'
-}
-
 export default s;
